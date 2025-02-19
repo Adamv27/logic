a4445c8112c4b476a7f2eff74dc2202b4d8df673
Adam Vinch <adamvinch@gmail.com>
2025-02-18 22:44:57 -0500

    Not gate

---
+++ src/notGate.js
@@ -0,0 +1,16 @@
+import Gate from "./gate.js";
+
+export default class NotGate extends Gate {
+  constructor() {
+    super(200, 275, 75, 1, 1);
+    this.color = "#DB5856";
+    this.name = "NOT";
+  }
+
+  propagateSignal() {
+    const connector0 = this.inputs[0];
+
+    const output = this.outputs[0];
+    output.propagateSignal(!connector0.value);
+  }
+}
